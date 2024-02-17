d179238668f84c116b5722a6861fad832dfa16f1
ravi-dholariya <200540107103@darshan.ac.in>
2024-02-17 16:27:40 +0530

    removed print filedetails function.

--- Business/FolderCopyService_WorkerClass.cs
+++ Business/FolderCopyService_WorkerClass.cs
@@ -263,21 +263,5 @@ namespace FolderCopyService.Business
             }
         }
         #endregion
-
-        #region Print FileDetails
-        public void PrintFileDetails(List<FileDetailsModel> list)
-        {
-            foreach (var item in list)
-            {
-                WriteToFile($"FolderId: {item.FolderId}");
-                WriteToFile($"FileName: {item.FileName}");
-                WriteToFile($"FileExtension: {item.FileExtension}");
-                WriteToFile($"FileStatus: {item.FileStatus}");
-                WriteToFile("---------------------------------------------\n");
-
-            }
-        }
-
-        #endregion
     }
 }
